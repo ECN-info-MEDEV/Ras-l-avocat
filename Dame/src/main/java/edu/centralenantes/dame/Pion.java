92e8eaafa0f16930dac7e6b80fb5f6b23facb064
MeryamAssermouh <95424934+MeryamAssermouh@users.noreply.github.com>
2022-12-15 11:41:13 +0100

    Update Pion.java

--- Dame/src/main/java/edu/centralenantes/dame/Pion.java
+++ Dame/src/main/java/edu/centralenantes/dame/Pion.java
@@ -36,6 +36,55 @@ public class Pion {
                     if(this.couleur.equals('noir')){
                         System.out.println('mouvement impossible');
                 }
+                    else(){
+                        this.manger()
+                    }
+                }
+                //si la case contient un pion blanc
+                if(p.plateau[this.pos.x-1][this.pos.y+1]==1){
+                    if(this.couleur.equals('blanc')){
+                        System.out.println('mouvement impossible');
+                }
+                    else(){
+                        this.manger()
+                    }
+                }
+            }
+        }
+        if(direction.equals('d')){
+            // verifier que la case est dans le plateau
+            if((this.pos.x+1<50)&&(this.pos.y+1<50)){
+                //Si la case est vide
+                if(p.plateau[this.pos.x+1][this.pos.y+1]==0){
+                
+                    if(this.couleur.equals('noir')){
+                        p.plateau[this.pos.x+1][this.pos.y+1] = -1;
+                }
+                    if(this.couleur.equals('blanc')){
+                        p.plateau[this.pos.x+1][this.pos.y+1] = 1;
+                }
+                    p.plateau[this.pos.x][this.pos.y]==0
+                    this.pos.x = this.pos.x+1;
+                    this.pos.y = this.pos.y+1;
+                
+                }
+                //si la case contient un pion noire
+                if(p.plateau[this.pos.x+1][this.pos.y+1]==-1){
+                    if(this.couleur.equals('noir')){
+                        System.out.println('mouvement impossible');
+                }
+                    else(){
+                        this.manger()
+                    }
+                }
+                //si la case contient un pion blanc
+                if(p.plateau[this.pos.x+1][this.pos.y+1]==1){
+                    if(this.couleur.equals('blanc')){
+                        System.out.println('mouvement impossible');
+                }
+                    else(){
+                        this.manger()
+                    }
                 }
             }
         }
