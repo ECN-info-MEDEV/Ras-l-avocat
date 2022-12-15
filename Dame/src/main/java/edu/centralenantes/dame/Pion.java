4827092797610f7ab8dc434709908b9d3583abbb
Meryam <meryam.assermouh@eleves.ec-nantes.fr>
2022-12-15 11:26:08 +0100

    deplace

--- Dame/src/main/java/edu/centralenantes/dame/Pion.java
+++ Dame/src/main/java/edu/centralenantes/dame/Pion.java
@@ -9,5 +9,35 @@ package edu.centralenantes.dame;
  * @author T480
  */
 public class Pion {
-    
+    public Pion(){
+        
+    }
+    public void deplace(String direction, Plateau p){
+        
+        if(direction.equals('g')){
+            // verifier que la case est dans le plateau
+            if((this.pos.x-1>=0)&&(this.pos.y+1<50)){
+                //Si la case est vide
+                if(p.plateau[this.pos.x-1][this.pos.y+1]==0){
+                
+                    if(this.couleur.equals('noir')){
+                        p.plateau[this.pos.x-1][this.pos.y+1] = -1;
+                }
+                    if(this.couleur.equals('blanc')){
+                        p.plateau[this.pos.x-1][this.pos.y+1] = 1;
+                }
+                    p.plateau[this.pos.x][this.pos.y]==0
+                    this.pos.x = this.pos.x-1;
+                    this.pos.y = this.pos.y+1;
+                
+                }
+                //si la case contient un pion noire
+                if(p.plateau[this.pos.x-1][this.pos.y+1]==-1){
+                    if(this.couleur.equals('noir')){
+                        System.out.println('mouvement impossible');
+                }
+                }
+            }
+        }
+    }
 }
