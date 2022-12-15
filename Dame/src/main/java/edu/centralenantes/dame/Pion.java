43c10bca9677145056cc01baa08cfd8313e93272
ELBETTALO <112825315+ELBETTALO@users.noreply.github.com>
2022-12-15 12:11:08 +0100

    Update Pion.java

--- Dame/src/main/java/edu/centralenantes/dame/Pion.java
+++ Dame/src/main/java/edu/centralenantes/dame/Pion.java
@@ -9,94 +9,95 @@ package edu.centralenantes.dame;
  * @author T480
  */
 public class Pion {
-    public Point2D pos; //position du pion 
-    public String couleur;
-    
-    
-    /**
-     * 
-     * @param position
-     * @param clr 
-     */
-    public Pion(Point2D position, String clr){
-        pos = position;
-        couleur = clr;
+
+    public Point2D pos;
+    public int couleur; // 1 pour blanche et -1 pour noir
+
+    public Pion(Point2D pos, int couleur) {
+        this.pos = pos;
+        this.couleur = couleur;
     }
-    public void deplace(String direction, Plateau p){
+
+   public void deplace(String direction, Plateau p){
         
         if(direction.equals('g')){
             // verifier que la case est dans le plateau
-            if((this.pos.x-1>=0)&&(this.pos.y+1<50)){
+            if((this.pos.getX()-1>=0)&&(this.pos.getY()+1<50)){
                 //Si la case est vide
-                if(p.plateau[this.pos.x-1][this.pos.y+1]==0){
+                if(p.plateau[this.pos.getX()-1][this.pos.getY()+1]==0){
                 
-                    if(this.couleur.equals('noir')){
-                        p.plateau[this.pos.x-1][this.pos.y+1] = -1;
+                    if(this.couleur == 1){
+                        p.plateau[this.pos.getX()-1][this.pos.getY()+1] = -1;
                 }
-                    if(this.couleur.equals('blanc')){
-                        p.plateau[this.pos.x-1][this.pos.y+1] = 1;
+                    if(this.couleur== -1){
+                        p.plateau[this.pos.getX()-1][this.pos.getY()+1] = 1;
                 }
-                    p.plateau[this.pos.x][this.pos.y]==0
-                    this.pos.x = this.pos.x-1;
-                    this.pos.y = this.pos.y+1;
+                    p.plateau[this.pos.getX()][this.pos.getY()]=0 ;
+                    this.pos.setX(this.pos.getX() - 1);
+                     this.pos.setY(this.pos.getY() + 1);
                 
                 }
                 //si la case contient un pion noire
-                if(p.plateau[this.pos.x-1][this.pos.y+1]==-1){
-                    if(this.couleur.equals('noir')){
-                        System.out.println('mouvement impossible');
+                if(p.plateau[this.pos.getX()-1][this.pos.getY()+1]==-1){
+                    if(this.couleur== -1){
+                        System.out.println("mouvement impossible");
                 }
-                    else(){
-                        this.manger()
+                    else{
+                        //this.manger() ;
                     }
                 }
                 //si la case contient un pion blanc
-                if(p.plateau[this.pos.x-1][this.pos.y+1]==1){
-                    if(this.couleur.equals('blanc')){
-                        System.out.println('mouvement impossible');
+                if(p.plateau[this.pos.getX()-1][this.pos.getY()+1]==1){
+                    if(this.couleur== 1){
+                        System.out.println("mouvement impossible");
                 }
-                    else(){
-                        this.manger()
+                    else{
+                        //this.manger()
                     }
                 }
             }
         }
         if(direction.equals('d')){
             // verifier que la case est dans le plateau
-            if((this.pos.x+1<50)&&(this.pos.y+1<50)){
+            if((+1<50)&&(this.pos.getY()+1<50)){
                 //Si la case est vide
-                if(p.plateau[this.pos.x+1][this.pos.y+1]==0){
+                if(p.plateau[this.pos.getX()+1][this.pos.getY()+1]==0){
                 
-                    if(this.couleur.equals('noir')){
-                        p.plateau[this.pos.x+1][this.pos.y+1] = -1;
+                    if(this.couleur== -1){
+                        p.plateau[this.pos.getX()+1][this.pos.getY()+1] = -1;
                 }
-                    if(this.couleur.equals('blanc')){
-                        p.plateau[this.pos.x+1][this.pos.y+1] = 1;
+                    if(this.couleur== 1){
+                        p.plateau[this.pos.getX()+1][this.pos.getY()+1] = 1;
                 }
-                    p.plateau[this.pos.x][this.pos.y]==0
-                    this.pos.x = this.pos.x+1;
-                    this.pos.y = this.pos.y+1;
+                    p.plateau[this.pos.getX()][this.pos.getY()]=0 ;
+                    this.pos.setX(this.pos.getX() + 1);
+                     this.pos.setY(this.pos.getY() - 1);
                 
                 }
                 //si la case contient un pion noire
-                if(p.plateau[this.pos.x+1][this.pos.y+1]==-1){
-                    if(this.couleur.equals('noir')){
-                        System.out.println('mouvement impossible');
+                if(p.plateau[this.pos.getX()+1][this.pos.getY()+1]==-1){
+                    if(this.couleur== -1){
+                        System.out.println("mouvement impossible");
                 }
-                    else(){
-                        this.manger()
+                    else{
+                        // this.manger();
                     }
                 }
                 //si la case contient un pion blanc
-                if(p.plateau[this.pos.x+1][this.pos.y+1]==1){
-                    if(this.couleur.equals('blanc')){
-                        System.out.println('mouvement impossible');
+                if(p.plateau[this.pos.getX()+1][this.pos.getY()+1]==1){
+                    if(this.couleur== 1){
+                        System.out.println("mouvement impossible");
                 }
-                    else(){
-                        this.manger()
+                    else{
+                        //this.manger();
                     }
                 }
             }
         }
     }
+    
+    
+    
+    
+    
 }
