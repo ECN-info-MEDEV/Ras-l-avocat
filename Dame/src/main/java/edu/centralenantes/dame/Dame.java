dc11b2dcce73babf5606590f40d4f4aa14e945ac
samar <elayedsamar@gmail.com>
2022-12-17 12:04:19 +0100

    test1

--- Dame/src/main/java/edu/centralenantes/dame/Dame.java
+++ Dame/src/main/java/edu/centralenantes/dame/Dame.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
@@ -12,8 +13,10 @@ package edu.centralenantes.dame;
 public class Dame {
 int[][] Mat = new int[10][10];
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!xy ");
     }
     
 }
+
